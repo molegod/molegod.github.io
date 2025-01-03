7f2f044f4173d1f5f06af55157be1684893277d8
molegod <ph475@cornell.edu>
2025-01-03 14:52:17 -0500

    made particles interactive

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ particlesJS("particles-js", {
       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
     },
     size: {
-      value: 3,
+      value: 2.5,
       random: true,
       anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
     },
@@ -37,36 +37,20 @@ particlesJS("particles-js", {
     }
   },
   interactivity: {
-    detect_on: "canvas",
+    detect_on: "window",
     events: {
-      onhover: { enable: false, mode: "repulse" },
-      onclick: { enable: false, mode: "push" },
+      onhover: { enable: true, mode: "bubble" },
+      onclick: { enable: true, mode: "repulse" },
       resize: true
     },
     modes: {
-      grab: { distance: 400, line_linked: { opacity: 1 } },
-      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 200, duration: 0.4 },
-      push: { particles_nb: 4 },
+      grab: { distance: 100, line_linked: { opacity: 0.75 } },
+      bubble: { distance: 100, size: 2.75, duration: 4, opacity: 0.75, speed: 10 },
+      repulse: { distance: 150, duration: 3 },
+      push: { particles_nb: 2 },
       remove: { particles_nb: 2 }
     }
   },
   retina_detect: true
 });
-var count_particles, stats, update;
-stats = new Stats();
-stats.setMode(0);
-stats.domElement.style.position = "absolute";
-stats.domElement.style.left = "0px";
-stats.domElement.style.top = "0px";
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector(".js-count-particles");
-update = function () {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
 requestAnimationFrame(update);
